047120e2a9e6c15edb1d11aaf545defc250d547f
Jorge Leon <84067777+jorge2751@users.noreply.github.com>
2023-02-18 08:52:59 -0800

    Added the last UD of CRUD features

--- server.py
+++ server.py
@@ -3,12 +3,44 @@ from user import User
 
 app = Flask(__name__)
 
+# Index page shows all users
 @app.route('/')
-def read():
+def index():
     users = User.get_all()
     return render_template('read.html', users=users)
 
-@app.route('/create', methods=['GET', 'POST'])
+# Show page shows one user
+@app.route('/users/<int:id>')
+def show(id):
+    user = User.get_by_id(id)
+    return render_template('show.html', user=user)
+
+# Edit page shows a form to edit a user
+@app.route('/users/<int:id>/edit')
+def edit(id):
+    user = User.get_by_id(id)
+    return render_template('edit.html', user=user)
+
+# Update action updates a user in the database
+@app.route('/users/<int:id>', methods=['POST'])
+def update(id):
+    data = {
+        'id': id,
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email']
+    }
+    User.save(data)
+    return redirect('/users/{}'.format(id))
+
+# Delete action deletes a user from the database
+@app.route('/users/<int:id>/delete', methods=['POST'])
+def delete(id):
+    User.delete(id)
+    return redirect('/')
+
+# Create page shows a form to create a user
+@app.route('/users/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
         data = {
@@ -16,10 +48,10 @@ def create():
             'last_name': request.form['last_name'],
             'email': request.form['email']
         }
-        User.save(data)
+        id = User.save(data)
         return redirect('/')
     else:
         return render_template('create.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- user.py
+++ user.py
@@ -3,7 +3,7 @@ from mysqlconnection import connectToMySQL
 class User:
     DB = "users_schema"
 
-    def __init__( self , data ):
+    def __init__(self, data):
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
@@ -13,11 +13,14 @@ class User:
 
     @classmethod
     def save(cls, data):
-        query = """
+        if 'id' in data:
+            return cls.update(data)
+        else:
+            query = """
                 INSERT INTO users (first_name, last_name, email)
                 VALUES (%(first_name)s, %(last_name)s, %(email)s);
-        """
-        return connectToMySQL(cls.DB).query_db(query, data)
+            """
+            return connectToMySQL(cls.DB).query_db(query, data)
 
     @classmethod
     def get_all(cls):
@@ -25,5 +28,34 @@ class User:
         results = connectToMySQL(cls.DB).query_db(query)
         users = []
         for user in results:
-            users.append( cls(user) )
+            users.append(cls(user))
         return users
+
+    @classmethod
+    def get_by_id(cls, id):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        data = {'id': id}
+        result = connectToMySQL(cls.DB).query_db(query, data)
+        if result:
+            return cls(result[0])
+        return None
+
+    @classmethod
+    def update(cls, data):
+        query = """
+            UPDATE users SET
+                first_name = %(first_name)s,
+                last_name = %(last_name)s,
+                email = %(email)s,
+                updated_at = NOW()
+            WHERE id = %(id)s;
+        """
+        connectToMySQL(cls.DB).query_db(query, data)
+        return data['id']
+
+    @classmethod
+    def delete(cls, id):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        data = {'id': id}
+        connectToMySQL(cls.DB).query_db(query, data)
+
